366ce54378912cd6648ed0388ce1ea11d4e89381
fieldplowing <fieldplowing@gmail.com>
2024-11-21 10:12:30 +0900

    Version 18.2.2 -> 19.0.0

--- projects/app-18/src/app/taskdetail/taskdetail.component.ts
+++ projects/app-18/src/app/taskdetail/taskdetail.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Control, StatusService } from 'lib-18';
+import { Item, StatusItemService } from 'lib-18';
 import { Location } from '@angular/common';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-taskdetail',
@@ -20,108 +21,52 @@ import { Location } from '@angular/common';
     MatButtonModule,
     MatSelectModule,
     MatRadioModule,
-    MatCardModule,
+    MatIconModule,
     ReactiveFormsModule,
   ],
 })
 export class TaskdetailComponent {
+  // ViewChild
+  @ViewChild('autosize') autosize!: CdkTextareaAutosize;
+  // properties
+  // show: boolean = false;
+
   private fb = inject(FormBuilder);
-  addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [
-      null,
-      Validators.compose([
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(5),
-      ]),
-    ],
-    shipping: ['free', Validators.required],
+  editForm = this.fb.group({
+    title: ['', Validators.required],
+    content: [''],
+    status: [''],
+    category: [''],
   });
 
-  hasUnitNumber = false;
-
-  states = [
-    { name: 'Alabama', abbreviation: 'AL' },
-    { name: 'Alaska', abbreviation: 'AK' },
-    { name: 'American Samoa', abbreviation: 'AS' },
-    { name: 'Arizona', abbreviation: 'AZ' },
-    { name: 'Arkansas', abbreviation: 'AR' },
-    { name: 'California', abbreviation: 'CA' },
-    { name: 'Colorado', abbreviation: 'CO' },
-    { name: 'Connecticut', abbreviation: 'CT' },
-    { name: 'Delaware', abbreviation: 'DE' },
-    { name: 'District Of Columbia', abbreviation: 'DC' },
-    { name: 'Federated States Of Micronesia', abbreviation: 'FM' },
-    { name: 'Florida', abbreviation: 'FL' },
-    { name: 'Georgia', abbreviation: 'GA' },
-    { name: 'Guam', abbreviation: 'GU' },
-    { name: 'Hawaii', abbreviation: 'HI' },
-    { name: 'Idaho', abbreviation: 'ID' },
-    { name: 'Illinois', abbreviation: 'IL' },
-    { name: 'Indiana', abbreviation: 'IN' },
-    { name: 'Iowa', abbreviation: 'IA' },
-    { name: 'Kansas', abbreviation: 'KS' },
-    { name: 'Kentucky', abbreviation: 'KY' },
-    { name: 'Louisiana', abbreviation: 'LA' },
-    { name: 'Maine', abbreviation: 'ME' },
-    { name: 'Marshall Islands', abbreviation: 'MH' },
-    { name: 'Maryland', abbreviation: 'MD' },
-    { name: 'Massachusetts', abbreviation: 'MA' },
-    { name: 'Michigan', abbreviation: 'MI' },
-    { name: 'Minnesota', abbreviation: 'MN' },
-    { name: 'Mississippi', abbreviation: 'MS' },
-    { name: 'Missouri', abbreviation: 'MO' },
-    { name: 'Montana', abbreviation: 'MT' },
-    { name: 'Nebraska', abbreviation: 'NE' },
-    { name: 'Nevada', abbreviation: 'NV' },
-    { name: 'New Hampshire', abbreviation: 'NH' },
-    { name: 'New Jersey', abbreviation: 'NJ' },
-    { name: 'New Mexico', abbreviation: 'NM' },
-    { name: 'New York', abbreviation: 'NY' },
-    { name: 'North Carolina', abbreviation: 'NC' },
-    { name: 'North Dakota', abbreviation: 'ND' },
-    { name: 'Northern Mariana Islands', abbreviation: 'MP' },
-    { name: 'Ohio', abbreviation: 'OH' },
-    { name: 'Oklahoma', abbreviation: 'OK' },
-    { name: 'Oregon', abbreviation: 'OR' },
-    { name: 'Palau', abbreviation: 'PW' },
-    { name: 'Pennsylvania', abbreviation: 'PA' },
-    { name: 'Puerto Rico', abbreviation: 'PR' },
-    { name: 'Rhode Island', abbreviation: 'RI' },
-    { name: 'South Carolina', abbreviation: 'SC' },
-    { name: 'South Dakota', abbreviation: 'SD' },
-    { name: 'Tennessee', abbreviation: 'TN' },
-    { name: 'Texas', abbreviation: 'TX' },
-    { name: 'Utah', abbreviation: 'UT' },
-    { name: 'Vermont', abbreviation: 'VT' },
-    { name: 'Virgin Islands', abbreviation: 'VI' },
-    { name: 'Virginia', abbreviation: 'VA' },
-    { name: 'Washington', abbreviation: 'WA' },
-    { name: 'West Virginia', abbreviation: 'WV' },
-    { name: 'Wisconsin', abbreviation: 'WI' },
-    { name: 'Wyoming', abbreviation: 'WY' },
-  ];
-  private statusService: StatusService = inject(StatusService);
-  private status: Control = this.statusService.value('task');
+  private statusService = inject(StatusItemService);
+  status = this.statusService.status();
+  data = this.statusService.data();
+  private task = {} as Item;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private location: Location,
-  ) {}
+  ) {
+    // 対象のDataをFormにパッチする
+    this.data.map((v) => {
+      if (v.id === this.status.SelectedRow) {
+        this.editForm.patchValue(v);
+      }
+    });
+  }
 
   onSubmit(): void {
-    // alert('Thanks!');
-    // ルートパラメータでページ遷移
-    this.location.back();
+    // 一時Dataの更新
 
-    // this.router.navigate(['/first']);
+    // リストに戻る
+    this.goBack();
+  }
+  goBack(): void {
+    // リストに戻る
+    this.location.back();
+    // ルートパラメータでページ遷移
+    // this.router.navigate(['memolist']);
   }
 }
--- projects/lib-18/src/lib/model/item.ts
+++ projects/lib-18/src/lib/model/item.ts
@@ -1,4 +1,5 @@
 export interface Item {
+  _id: string;
   id: number;
   status: string;
   category: string;
